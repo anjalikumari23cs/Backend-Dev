71440906070fab7f08f51d0775ddf52fe4da7da8
anjalikumari23cs <anjalikumari93680@gmail.com>
2026-01-08 16:03:52 +0530

    Js Practice

---
+++ script.js
@@ -0,0 +1,27 @@
+// const user={name:'Rahul',email:'rahul@gmail.com',phone:'234543532'}
+// const username=user.name;
+// const useremail=user.email;
+// const {name, email, phone, social}=user;
+
+// const user1=user;
+// user1.name='Ajay';
+// // console.log(user.name);
+
+// const numbers=[1,2,3,4,5];
+// const newnumbers=numbers.map((number)=>number*2);
+// console.log(newnumbers);
+
+// const sumofnumbers=numbers.reduce((sum,number)=>sum+number,0);{
+// console.log(sumofnumbers);
+// }
+
+// const rawUsers = [
+// { id: 1, name: "Rahul", password: "fb_password", role: "admin" },
+// { id: 2, name: "Sanya", password: "123_password", role: "user" },
+// { id: 3, name: "Amit", password: "secret_password", role: "user" }
+// ];
+
+// const safeuser=rawUsers.map(({password,...rest})=>rest);
+// console.log(safeuser);
+// const admin=safeuser.find((user)=>user.role==='admin');
+// console.log(admin);
