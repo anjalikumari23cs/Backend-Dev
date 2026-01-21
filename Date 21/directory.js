632b1fd936b8a871e5cc38be428ec71cf0288c52
anjalikumari23cs <anjalikumari93680@gmail.com>
2026-01-21 14:50:23 +0530

    Creating directory, deleting

--- Date 21/directory.js
+++ Date 21/directory.js
@@ -1,11 +1,40 @@
 const fs=require('fs');
-//create a directory
-fs.mkdirSync("newDirectory");
-fs.mkdir("folder/folder1/folder2",{recursive:true},(err)=>{
+// create a directory
+// fs.mkdir("newDirectory",(err)=>{
+//     if(err){
+//         return;
+//     }
+//     console.log("Directory is created");
+// });
+// fs.mkdir("folder/folder1/folder2",{recursive:true},(err)=>{
+//     if(err){
+//         console.log("error"); 
+//         return;
+//     }
+//     console.log("Directory is created");
+    
+// });
+
+fs.readdir("newDirectory",(err,files)=>{
     if(err){
-        console.log("error"); 
+        console.log("Error");
         return;
     }
-    console.log("Directory is created");
-    
+    console.log(files);
+});
+
+fs.rmdir("newDirectory",(err)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log("Directory deleted successfully");
+});
+
+fs.rm("newDirectory",(err)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log("Directory deleted successfully");
 });
